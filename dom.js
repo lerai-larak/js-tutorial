94b5fa092e1fcab4ef1ee65d200c9fd8bcb25241
Kelly Muhindi <kmuhindi@bandahealth.org>
2019-03-14 08:58:31 +0300

    Worked with element  attributes

--- dom.js
+++ dom.js
@@ -93,3 +93,132 @@ var addedNode = body.appendChild(clone);
 //normalize(): deals with text nodes in a document subtree. Different DOM parser
 //implementations cause some text nodes to exist. This method searches and remov
 //es empty text nodes in a specified node and its descendants.
+//
+//Document type
+//document is an instance of HTMLDocument which inherits from Document.
+//A Document type can represent HTML or XML documents. 
+//document represents the entire HTML page.
+//is a property of window and so is accessible globally.
+var html = document.documentElement;
+var html = document.childNodes[0];
+var html = document.firstChild;
+
+var body = document.body;
+var title = document.title;
+document.title = "Another Title"; //sets the document title
+var url = document.URL; //gets complete url of loaded page
+var domain = document.domain; //gets the domain 
+var referrer = document.referrer; //gets the page that linked to this page 
+
+var paragraph = document.getElementById("para1"); //returns the element referenced by the id
+//null if no element is found. The id is case sensitive. If more than 1 element on the page 
+//with the same id, the method returns the first element found.
+//IE7 and ealier will return a form element whose name attribute matches the id
+//if that element occurs first in the page. (wierd).
+
+var paragraphs = document.getElementByTagName("p"); //Returns a nodelist (HTMLCollection)
+//of all elements on page matching the tag.
+var count = paragraphs.count; //number of paragraphs on the doc
+var paragraph1 = paragraphs[0]; //get the first paragraph.
+
+var radioBtns = document.getElementByName("color"); //get all elements with the 
+name attribute specified. Most used on radio buttons.
+
+//special collections
+document.anchors; //returns all <a> elements with a name attribute in the document
+document.forms; //contains all <form> elements in the document. same as:
+document.getElementsByTagName("form");
+document.images: //contains all the <img> elements in the document 
+document.links; //contains all the <a> elements with a href attribute in the doc
+//the above collections are contantly updated to match the content of the
+//current document.
+//
+//writting to a document
+document.write("some text");
+document.writeln("some text followed by a new line");
+//The methods are oftern used to include external js on a page
+//
+<script type="text/javascript">
+    document.write("<script type=\"text/javascript\" src=\"file.js\">"+"</script>");
+</script>
+//the above is incorrect since js will interpret the script tag in the string as 
+  //the closing script tag, to avoid this the script tag inside the string should
+  //be escaped ie <\/script>
+  //
+
+//Element type: Represents an HTML or XML element
+  //has nodeType nodeName, nodeValue, parentNode and childNodes
+  var elem = document.getElementById("p");
+  var nodeName1 = elem.nodeName;
+  var nodeName2 = elem.tagName;
+  nodeName1 === nodeName2; //true
+//nb: nodeName and tagName always outputs the name in uppercase.
+//
+//HTMLElement type Represents all HTML elements directly or through subtyping
+//It inherits from Element and adds several props.
+//Has properties: id, title, lang, dir, className
+
+div.id; //get id
+div.className; //get the class
+div.className = "sub"; //changes class attribute of div to 'sub'
+div.title = 'a-title'; //change title property of div
+
+//getting and setting attributes
+var myDiv = document.getElementById('myDiv');
+var divClass = myDiv.getAttribute('class');
+//can also be used to fetch custom attributes 
+var value = div.getAttribute('my-special-attrib');
+
+//attribute names are case insensitive ID and id are the same
+//custom properties in HTML5 are prepended with a data- in order to validate
+//attributes are available as DOM but custom elements do not show up as 
+//properties of the DOM element.
+//NB:style attribute accessed via getAttribute() returns css text, when accessed
+//through the dom property it returns an object.
+//
+//setting attributes
+//setAttribute(); accepts 2 args; name of the attribute and and value to set to it
+//If attribute exists, it value is replaced, if not the attrib is created and value
+//set.
+div.setAttribute('class', 'anotherClass');
+//Works for html attributes and custom attributes.
+//All attributes are properties and can be set directly 
+div.class = 'someOtherClass';
+
+//removing attributes
+//removes the attribute from the element altogether(both attribute and value)
+div.removeAttribute('class');
+
+//The attributes property
+//Used only in the Element DOM node type. attribute property has a NamedNodeMap, 
+//a live collection of nodes.
+div1.attributes; //returns a NamedNodeMap (collection of all attributes in element)
+//Every attribute on an element is represented by an
+//Attr node, each of which is stored in the NamedNodeMap object. Methods in the 
+//object
+//getNamedItem(name); //return node whose nodeName prop is equal to name
+//removeNamedItem(name); //remove node whose nodeName prop is equal to name
+//setNamedItem(node); //adds node to the list, indexing ig by its nodeName property
+var id = div.attributes.getNamedItem('id').nodeValue;
+var id = div.attributes["id"].nodeValue; //shorthand
+//setting values
+div.attributes.getNamedItem('id').nodeValue = "someOtherId";
+
+//iterate over an element's nodes attributes
+
+function outputAttributes(element){
+  var pairs = new Array(),
+    attrName,
+    attrValue,
+    i,
+    len;
+
+  for (i = 0, len=element.attributes.length; i < len; i++){
+    attrName = element.attributes[i].nodeName;
+    attrValue = element.attributes[i].nodeValue;
+    pairs.push(attrName + "=\"" + attrValue + "\"");
+  }
+  return pairs.join(" ");
+}
+
+
