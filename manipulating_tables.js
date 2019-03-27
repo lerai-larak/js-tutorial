76a809d617afc6a86e8f09f1f053668772af5fbe
Kelly Muhindi <kmuhindi@bandahealth.org>
2019-03-27 07:55:56 +0300

    Add how to create tables dynamically

--- manipulating_tables.js
+++ manipulating_tables.js
@@ -1,1 +1,32 @@
-dsd
+//HTML DOM provides extra methods for the creation of tables
+//create table
+var table = document.createElement("table");
+table.border = 1;
+table.width = "100%";
+
+//create the tdody
+var tbody = document.createElement("tbody");
+table.appendChild(tbody);
+
+//create the first row
+tbody.insertRow(0);
+tbody.rows[0].insertCell(0);
+tbody.rows[0].cells[0].appendChild(document.createTextNode("Cell 1,1"));
+tbody.rows[0].insertCell(1);
+tbody.rows[0].cells[0].appendChild(document.createTextNode("Cell 2,1"));
+
+//create the second row
+tbody.insertRow(1);
+tbody.rows[0].insertCell(0);
+tbody.rows[0].cells[0].appendChild(document.createTextNode("Cell 1,2"));
+tbody.rows[0].insertCell(1);
+tbody.rows[0].cells[0].appendChild(document.createTextNode("Cell 2,2"));
+
+document.body.append(table);
+
+
+
+
+
+
+
