c47975dfa3ac2ff130c348abc4555085bbb48c7d
Kelly Muhindi <kmuhindi@bandahealth.org>
2019-05-13 19:58:42 +0300

    Started on js events

---
+++ events/events.js
@@ -0,0 +1,11 @@
+//Events
+//standardized in the DOM Level 2 spec
+//Event flow: order in which events are received on the page.
+//Event bubbling: An approach adopted in handling events where, 
+//an event is said to start at the most specific element and flow upwards
+//towards the least specific node in the document.
+//In the html page the click event will be fired on the div then body then html 
+//and finally the document node.
+//
+//Event capturing works in the opposite way
+//
