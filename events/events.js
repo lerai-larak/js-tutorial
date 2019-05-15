88ed82b042571cba8c0f5e7d8cc24fc601b7901a
Kelly Muhindi <kmuhindi@bandahealth.org>
2019-05-15 18:17:08 +0300

    About scope augmentation and DOM Level 0 event handlers

--- events/events.js
+++ events/events.js
@@ -15,3 +15,28 @@
 function showMsg(){
   alert('Event type: ' + event.type + '\n' + 'Element id: ' + event.target.id);
 }
+
+//by augmenting the scope chain using the with statement
+//members of both document and the element can be accessed as if they are local 
+//variables. See onlick event handler on the form.
+//
+//Event handlers should be in try-catch blocks for cases where the user interacts 
+//with an element before the handler is fully defined on the page.
+//
+//DOM Level 0 Event Handlers
+//Altenative to adding handers from HTML elements
+//All elements have event handler properties which can be assigned properties equal 
+//to a function.
+var button = document.getElementById('btn1');
+button.onclick = function(){
+  	alert('Clicked button');
+}
+//Using this method, the function is considered to belong to the element and so
+//runs within the scope of the element and the this attribute can be used.
+button.onclick = function(){
+  console.log(this.id); //logs the id of the attached element
+}
+
+button.onclick = null; //removes the event handler
+
+//DOM Level 2 Event handlers
